46ea6b208a221d12f7e0fc5b705ad000fd6478c8
khemrajshrestha471 <khemrajshrestha471@gmail.com>
2025-06-23 07:50:30 +0545

    imagekit project completed

---
+++ app/components/imp.ts
@@ -0,0 +1,22 @@
+// MONGODB_URI = mongodb+srv://imagekit-test:wZFzdT3VzlxDv6QM@imagekit-cluster.taci3ne.mongodb.net/
+
+// NEXTAUTH_SECRET = chaicode
+
+// # imagekit configs
+
+// NEXT_PUBLIC_PUBLIC_KEY = public_NM/V/WhV6gkkTAkQdR5XscRdRjA=
+
+// NEXT_PUBLIC_URL_ENDPOINT = https://ik.imagekit.io/1trmpqebn
+
+// IMAGEKIT_PRIVATE_KEY = private_VcAygIXsEdHVIzxNhfxXbNyWg/k=
+
+
+// username:- khemrajshrestha471
+// password:- SdkjHwOB0sYKaY6V
+
+
+
+// user:- imagekit-test
+// auto:- wZFzdT3VzlxDv6QM
+
+// ImageKit pwd:- BC@_Jq6C8czUCnL
